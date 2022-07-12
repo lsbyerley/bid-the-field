614770fc37aec30428a9121fb438f61236e2e45e
lsbyerley <lsbyerley@gmail.com>
2022-07-11 20:43:21 -0400

    open championship 2022

--- scripts/getPlayerData.mjs
+++ scripts/getPlayerData.mjs
@@ -74,11 +74,12 @@ export const fetchUsOpen = async (url) => {
 // -----------------------------------------
 // Open Championship Player Pool
 // found here: https://www.theopen.com/api/QualifiedPlayersListingApi/GetPlayersByPageAndFilters
+// https://statdata-api-prod.pgatour.com/api/clientfile/Field?T_CODE=r&T_NUM=100&YEAR=2022&format=json
 // -----------------------------------------
 
 export const fetchOpenChamp = async (url) => {
   const playerRes = await fetch(url).then((res) => res.json());
-  const players = playerRes.items
+  /* const players = playerRes.items
     .filter((p) => p.isPlaying)
     .map((p) => {
       return {
@@ -88,7 +89,18 @@ export const fetchOpenChamp = async (url) => {
         full_name: `${p.firstName} ${p.lastName}`,
         short_name: `${p.firstName.substring(0, 1)}. ${p.lastName}`,
       };
-    });
+    }); */
+  const players = playerRes.Tournament.Players.filter(
+    (p) => p.isAlternate === 'No'
+  ).map((p) => {
+    return {
+      id: p.TournamentPlayerId,
+      first_name: p.PlayerFirstName,
+      last_name: p.PlayerLastName,
+      full_name: `${p.PlayerFirstName} ${p.PlayerLastName}`,
+      short_name: `${p.PlayerFirstName.substring(0, 1)}. ${p.PlayerLastName}`,
+    };
+  });
   // Add The Field player obj
   players.unshift(theFieldPlayer);
   return players;
@@ -147,9 +159,11 @@ export const fetchPgaChamp = async (url) => {
 // https://statdata-api-prod.pgatour.com/api/clientfile/Field?T_CODE=r&T_NUM=480&YEAR=2022&format=json
 
 const run = async () => {
-  const players = await fetchUsOpen(
-    //'https://statdata-api-prod.pgatour.com/api/clientfile/Field?T_CODE=r&T_NUM=033&YEAR=2022&format=json'
-    'https://statdata-api-prod.pgatour.com/api/clientfile/Field?T_CODE=r&T_NUM=026&YEAR=2022&format=json'
+  const players = await fetchOpenChamp(
+    // 'https://statdata-api-prod.pgatour.com/api/clientfile/Field?T_CODE=r&T_NUM=033&YEAR=2022&format=json'
+    // 'https://statdata-api-prod.pgatour.com/api/clientfile/Field?T_CODE=r&T_NUM=026&YEAR=2022&format=json'
+    // 'https://www.theopen.com/api/QualifiedPlayersListingApi/GetPlayersByPageAndFilters'
+    'https://statdata-api-prod.pgatour.com/api/clientfile/Field?T_CODE=r&T_NUM=100&YEAR=2022&format=json'
   );
 
   const returnObj = {
