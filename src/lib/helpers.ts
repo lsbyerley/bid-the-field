ab47afaef944046b13ed714cdd749fb84211e1eb
lsbyerley <lsbyerley@gmail.com>
2023-05-07 10:36:00 -0400

    fix: type

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -5,7 +5,7 @@ export const getURL = () => {
     'http://localhost:3000/';
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
-  // Make sure to including trailing `/`.
+  // Make sure to include trailing `/`.
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
   return url;
 };
