b8c5af45c6c4a10d473f7cc7f531e1263d1f2fca
lsbyerley <lsbyerley@gmail.com>
2024-02-03 21:52:38 -0500

    chore: pkg updates 02032024

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from '../Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className='flex flex-col min-h-screen bg-base-200 grid-bg'>
+    <div className='flex flex-col min-h-screen bg-base-200'>
       <NavBar />
       <main className='flex-1'>{children}</main>
       <Footer />
